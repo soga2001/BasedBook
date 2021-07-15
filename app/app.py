11b322a5f6156f5953fbd92d2ab0eb180941478c
soga2001 <soga2058@gmail.com>
2021-07-14 22:36:46 -0600

    learning more mongodb

--- app/app.py
+++ app/app.py
@@ -7,31 +7,35 @@ from marshmallow.fields import Date
 from models.user import User
 from bson.objectid import ObjectId
 
+app = Flask(__name__)
 
+#connecting to mongo running on the computer
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test_database"
+    
+#using local reference
+mongo = PyMongo(app)
+
+@app.route("/")
+def hello():
+    return 'hello'
+
+
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        place = request.args.get('place', None)
+        if place:
+            return place
+        return "No place information is given"
 
 def create_app(env=None):
-    app = Flask(__name__)
-
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/test_database"
-
-    #connecting to mongo running on the computer
-    # mongodb_client = PyMongo("mongodb://localhost:27017/")
-     
-    #using local reference
-    mongo = PyMongo(app)
 
     @app.route("/users", methods = ['GET'])
     def get_users():
-        # user_list = []
-        # for user in mongo.db.users.find():
-        #     user_list.append({"name": user["name"] })
-        
-        #pythonic way to do things
+
         user_list = [User().dump(user_doc) for user_doc in mongo.db.users.find()]
 
-        # users = db.users.find()
         return jsonify(user_list)
-        # return jsonify("potato")
 
     @app.route("/users", methods = ['POST'])
     def post_user():
@@ -39,11 +43,11 @@ def create_app(env=None):
         data = request.json
 
         user = User().load(data)
+        print("user", user)
         result = mongo.db.users.insert_one(user)
         new_user = User().dump(mongo.db.users.find_one(user))
-        
-        # print("dumping", User().dump(result))
-        # print(result)
+
+
         return jsonify(new_user)
 
     @app.route("/users/<user_id>", methods = ['GET'])
@@ -55,18 +59,30 @@ def create_app(env=None):
 
     @app.route("/users/<user_id>", methods = ['DELETE'])
     def remove_user_by_id(user_id):
-        remove_user = User().dump(mongo.db.users.delete_one({"_id": ObjectId(user_id)}))
+        remove_user = User().dump(mongo.db.users.find_one_and_delete({"_id": ObjectId(user_id)}))
         
-        # print("Hello", remove_user)
+
         return jsonify(remove_user)
 
     @app.route("/users", methods = ['DELETE'])
     def remove_all():
         remove_all = User().dump(mongo.db.users.remove({}))
-        
-        # print("Hello", remove_user)
+
         return jsonify(remove_all)
+
+    @app.route("/users/<user_id>", methods = ['PUT'])
+    def update_user_by_id(user_id):
+
+        data = request.json
+
+        user = User().load(data)
+        
+        update_user = User().dump(mongo.db.users.find_one_and_update({"_id": ObjectId(user_id)}, {"$set": user}))
+
+        return jsonify(update_user, data)
 
     return app
 
-    
+
+if __name__ == '__main__':
+    app.run(debug=True)
