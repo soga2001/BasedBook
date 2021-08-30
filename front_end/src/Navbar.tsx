51b64036bd977c7145b3e6dd29d6c30622a8b61a
soga2001 <soga2058@gmail.com>
2021-08-29 22:27:30 -0600

    Changed from reactstrap to bootstrap and made Navbar better

--- front_end/src/Navbar.tsx
+++ front_end/src/Navbar.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import {
   Collapse,
-  Navbar,
-  NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText
-} from 'reactstrap';
+  Navbar, NavbarBrand, Nav, NavItem, NavLink, Container, NavDropdown
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function logout() {
@@ -14,45 +13,29 @@ function logout() {
 
 
 const NavBar = (props:any) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
-    <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/Home">Social Media</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="justify-content-center" navbar>
-          <NavItem>
-            <NavLink href="/Home">Home</NavLink>
-          </NavItem>
-          <NavItem className="right">
-            {!localStorage.getItem('token') && <NavLink href="/Login">Login</NavLink>}
-          </NavItem>
-          <NavItem>
-            {!localStorage.getItem('token') && <NavLink href="/Register">Register</NavLink> }
-          </NavItem>
-            {localStorage.getItem('token') &&
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                {localStorage.getItem('username')}
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem onClick={logout}>
-                  Logout
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown> }
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="/Home">Social Media</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="/Home">Home</Nav.Link>
+      {localStorage.getItem('token') && <Nav.Link href="/Post">Post</Nav.Link>}
+    </Nav>
+    <Nav>
+    {!localStorage.getItem('token') && <Nav.Link href="/Login">Login</Nav.Link>}
+    {!localStorage.getItem('token') && <Nav.Link href="/Register">Register</Nav.Link>}
+    {localStorage.getItem('token') &&
+      <NavDropdown title={localStorage.getItem('username')} id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+        </NavDropdown>}
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 }
 
